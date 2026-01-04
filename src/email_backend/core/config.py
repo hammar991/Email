0f224bdb464121f6d7ad5ab15325cfb307dff4b3
yy-crazy <yujiale20020131@163.com>
2026-01-04 21:38:48 +0800

    配置数据库

--- src/email_backend/core/config.py
+++ src/email_backend/core/config.py
@@ -2,16 +2,27 @@
 @File: config.py
 配置文件
 """
+import os
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    TITLE :str = "信箱项目"
+    # 项目基本信息
+    TITLE :str = "信箱项目2.0"
     API_V1_STR: str = "/api/v1"
     VERSION: str = "0.1.0"
+    PROJECT_NAME: str = "信箱系统"
+    DESCRIPTION: str = "这是一个信箱系统，支持用户使用多个信箱进行收发邮件及分享邮箱，技术栈:FastAPI + SQLModel + Sqlite"
+
+    # jwt访问令牌
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30       # 访问令牌过期时间
-    DATABASE_URL: str = "sqlite:///database.db"         # 数据库
-    ALGORITHM : str = "HS256"           # 算法
+    ALGORITHM: str = "HS256"  # 算法
+
+    # 数据库
+    DATABASE_URL: str = "sqlite:///C:/Users/user/Desktop/email/src/email_backend/schemes/database.db"
+
+    # 静态资源目录
+    STATIC_DIR: str = os.path.join(os.getcwd(), "../static")
 
 
 settings = Settings()
